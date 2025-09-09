e412f8ba3321050ba007656a369e44d400accd4a
Alistair <104738937+alistair-barfoot@users.noreply.github.com>
2025-09-09 15:33:29 -0400

    Fixed arguments and removed some unneccesary lines

--- Lab 1/src/lab1.py
+++ Lab 1/src/lab1.py
@@ -6,24 +6,24 @@ import torch
 import torchvision.transforms as transforms
 import argparse
 import matplotlib.pyplot as plt 
-import numpy as np
 from torchvision.datasets import MNIST
 from model import autoencoderMLP4Layer
 
 def main():
   # read arguments from command line
   argParser = argparse.ArgumentParser()
-  argParser.add_argument('-s', metavar='state', type=str, help='parameter file (.pth)')
+  argParser.add_argument('-l', metavar='state', type=str, help='parameter file (.pth)')
   argParser.add_argument('-z', metavar='bottleneck size', type=int, help='int [32]')
 
   args = argParser.parse_args()
 
   save_file = None
-  if args.s != None:
-      save_file = args.s
-  bottleneck_size = 0
+  if args.l != None:
+      save_file = args.l
+  bottleneck_size = 8
   if args.z != None:
       bottleneck_size = args.z
+  print('bottleneck size = ', bottleneck_size)
 
   device = 'cpu'
   if torch.cuda.is_available():
@@ -33,10 +33,8 @@ def main():
   train_transform = transforms.Compose([
       transforms.ToTensor()
   ])
-  test_transform = train_transform
 
   train_set = MNIST('./data/mnist', train=True, download=True, transform=train_transform)
-  test_set = MNIST('./data/mnist', train=False, download=True, transform=test_transform)
 
   N_input = 28 * 28   # MNIST image size 
   N_output = N_input
@@ -47,6 +45,7 @@ def main():
 
   idx = 0
   while idx >= 0:
+    # Get user input
     idx = input("Enter index > ")
     idx = int(idx)
     n = input("Enter number of iterations > ")
@@ -58,6 +57,7 @@ def main():
       img = img.type(torch.float32)
       img = (img - torch.min(img)) / torch.max(img)
       img = img.view(1, img.shape[0]*img.shape[1]).type(torch.FloatTensor)
+      # Run through model
       with torch.no_grad():
         output = model(img)
       img = img.view(28, 28).type(torch.FloatTensor)
@@ -80,17 +80,21 @@ def main():
       img = img.type(torch.float32)
       img = (img - torch.min(img)) / torch.max(img)
 
-      noise_factor = 0.3
+      # Add random noise to the image
+      noise_factor = 0.25
       noise = torch.rand(img.shape) * noise_factor
       noisy_img = img + noise
       noisy_img = torch.clamp(noisy_img, 0., 1.)
       noisy_img = noisy_img.to(device=device)
       noisy_img = noisy_img.view(1, noisy_img.shape[0]*noisy_img.shape[1]).type(torch.FloatTensor)
+
+      # Run through model
       with torch.no_grad():
         output = model(noisy_img)
       noisy_img = noisy_img.view(28, 28).type(torch.FloatTensor)
       output = output.view(28, 28).type(torch.FloatTensor)
 
+      # Output images
       f=plt.figure()
       f.add_subplot(1,3,1)
       plt.title('Input')
@@ -112,6 +116,8 @@ def main():
       img = (img - torch.min(img)) / torch.max(img)
       imgs = []
       imgs.append(img.clone())
+
+      # Add noise 'n' times and pass through model
       for i in range(n):
         if i > 0:
           img = imgs[i-1]
@@ -119,20 +125,23 @@ def main():
         img = (img - torch.min(img)) / torch.max(img)
 
         # Add random noise to the image
-        noise_factor = 0.2
+        noise_factor = 0.25
         noise = torch.rand(img.shape) * noise_factor
         img = img + noise
         img = torch.clamp(img, 0., 1.)
         img = img.to(device=device)
         img = img.view(1, img.shape[0]*img.shape[1]).type(torch.FloatTensor)
 
+        # Run through model
         with torch.no_grad():
           output = model(img.view(1, 28*28).to(device=device))
         output = output.view(28, 28).type(torch.FloatTensor)
 
         img = img.view(28, 28).type(torch.FloatTensor)
+        # Add image to list
         imgs.append(output)
       f = plt.figure()
+      # Plot each image
       for i in range(n):
         if i == 0:
           f.add_subplot(1,n,1)
