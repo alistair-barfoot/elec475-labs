da7d90cf1559fa106d54a3de54c1fe81d9f8d3c1
Alistair <104738937+alistair-barfoot@users.noreply.github.com>
2025-10-14 14:32:08 -0400

    Changed some things around and trained some models

--- Lab 2/runlist.bat
+++ Lab 2/runlist.bat
@@ -1,4 +1,4 @@
-py .\train.py -s 'snoutnet_weights_C.pth' -e 30 -b 256
-py .\train.py -s 'snoutnet_weights_D.pth' -e 30 -b 64
-py .\train.py -s 'snoutnet_weights_E.pth' -e 30 -b 32
-py .\train.py -s 'snoutnet_weights_F.pth' -e 30 -b 128
+py .\train.py -s snoutnet_weights_L.pth -e 30 -b 16
+py .\train.py -s snoutnet_weights_M.pth -e 30 -b 32
+py .\train.py -s snoutnet_weights_N.pth -e 30 -b 64
+py .\train.py -s snoutnet_weights_O.pth -e 30 -b 128
