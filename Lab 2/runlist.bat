68fed672b5b985425d11fbbd3544d941c828fde7
Alistair <104738937+alistair-barfoot@users.noreply.github.com>
2025-10-15 19:53:33 -0400

    Trained a model with some aug

--- Lab 2/runlist.bat
+++ Lab 2/runlist.bat
@@ -1,6 +1,6 @@
-py .\train.py -n -s snoutnet_weights_N.pth -p aug_plotN.png -e 30 -b 32
-py .\train.py -n -s snoutnet_weights_O.pth -p aug_plotO.png -e 30 -b 16
-py .\train.py -r -s snoutnet_weights_P.pth -p aug_plotP.png -e 30 -b 32
-py .\train.py -r -s snoutnet_weights_Q.pth -p aug_plotQ.png -e 30 -b 16
-py .\train.py -f -r -s snoutnet_weights_R.pth -p aug_plotR.png -e 30 -b 32
-py .\train.py -f -r -s snoutnet_weights_S.pth -p aug_plotS.png -e 30 -b 16
+py .\train.py -f -r -s snoutnet_weights_R.pth -p aug_plotR.png -e 30 -b 64
+py .\train.py -f -r -s snoutnet_weights_S.pth -p aug_plotS.png -e 30 -b 128
+py .\train.py -n -s snoutnet_weights_T.pth -p aug_plotT.png -e 30 -b 64
+py .\train.py -n -s snoutnet_weights_U.pth -p aug_plotU.png -e 30 -b 128
+py .\train.py -r -s snoutnet_weights_V.pth -p aug_plotV.png -e 30 -b 64
+py .\train.py -r -s snoutnet_weights_W.pth -p aug_plotW.png -e 30 -b 128
