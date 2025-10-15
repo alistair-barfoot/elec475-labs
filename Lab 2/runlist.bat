d30dfd3cac55038d1e9735cc56031673c5982b64
Alistair <104738937+alistair-barfoot@users.noreply.github.com>
2025-10-15 12:55:27 -0400

    trained some b16 and b32 models with aug

--- Lab 2/runlist.bat
+++ Lab 2/runlist.bat
@@ -1,4 +1,6 @@
-py .\train.py -s snoutnet_weights_L.pth -e 30 -b 16
-py .\train.py -s snoutnet_weights_M.pth -e 30 -b 32
-py .\train.py -s snoutnet_weights_N.pth -e 30 -b 64
-py .\train.py -s snoutnet_weights_O.pth -e 30 -b 128
+py .\train.py -n -s snoutnet_weights_N.pth -p aug_plotN.png -e 30 -b 32
+py .\train.py -n -s snoutnet_weights_O.pth -p aug_plotO.png -e 30 -b 16
+py .\train.py -r -s snoutnet_weights_P.pth -p aug_plotP.png -e 30 -b 32
+py .\train.py -r -s snoutnet_weights_Q.pth -p aug_plotQ.png -e 30 -b 16
+py .\train.py -f -r -s snoutnet_weights_R.pth -p aug_plotR.png -e 30 -b 32
+py .\train.py -f -r -s snoutnet_weights_S.pth -p aug_plotS.png -e 30 -b 16
