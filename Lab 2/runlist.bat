1af4373867e0cde8693d24b39e2102d6d830876e
lukeabarry <lukeabarry@gmail.com>
2025-10-26 14:18:12 -0400

    ur trained boy

--- Lab 2/runlist.bat
+++ Lab 2/runlist.bat
@@ -1,6 +1,4 @@
-py .\train.py -f -r -s snoutnet_weights_S.pth -p aug_plotS.png -e 30 -b 128
-py .\train.py -f -r -s snoutnet_weights_X.pth -p aug_plotX.png -e 30 -b 64
-py .\train.py -n -s snoutnet_weights_T.pth -p aug_plotT.png -e 30 -b 64
-py .\train.py -n -s snoutnet_weights_U.pth -p aug_plotU.png -e 30 -b 128
-py .\train.py -r -s snoutnet_weights_V.pth -p aug_plotV.png -e 30 -b 64
-py .\train.py -r -s snoutnet_weights_W.pth -p aug_plotW.png -e 30 -b 128
+py .\train.py -r -s sw_ROT_AUG.pth -p aug_plot_ROT.png -e 50 -b 32
+py .\train.py -f -s sw_FLIP_AUG.pth -p aug_plot_FLIP.png -e 50 -b 32
+py .\train.py -f -r -s sw_BOTH_AUG.pth -p aug_plot_BOTH.png -e 50 -b 32
+py .\train.py -s sw_NO_AUG.pth -p aug_plot_NO_AUG.png -e 50 -b 32
